c810af5a2dc6775b7e5ea5aa0ca2460fa813e20e
GurcanH <gurcanhamali@gmail.com>
2021-04-25 05:54:12 +1000

    Branch-1 Changes

--- test-b.js
+++ test-b.js
@@ -1,3 +1,15 @@
 // Codes codes 
 
 // Update codes
+
+
+// BR1 new lines
+// BR1 new lines
+// BR1 new lines
+const branch1_func1 = () => {
+    console.log(branch1)
+}
+
+const branch1_func2 = () => {
+    console.log(branch1 + 'ok')
+}
